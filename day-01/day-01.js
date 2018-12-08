f274db8fc2365fe706ac15348d50aeb61ce3af51
nephring <nephring@gmail.com>
2018-12-08 12:42:39 +0100

    add day-01 input

--- day-01/day-01.js
+++ day-01/day-01.js
@@ -1,19 +1,16 @@
 /*
 Day 01 Advent of Code
-
 https://adventofcode.com
 */
 
-const input =
-`+1
--2
-+40
--17
--10
-+20`
+const input = require('./input.js').input;
 
-convertedInput = input.split('\n').map(item => parseInt(item))
-const result = convertedInput.reduce((previousValue, currentValue) => previousValue + currentValue, 0)
+// convert input to array of numbers
+convertedInput = input.split('\n').map(item => parseInt(item));
+const result = convertedInput.reduce(
+  (previousValue, currentValue) => previousValue + currentValue,
+  0
+);
 
-
-console.log(result)
+// log result
+console.log(result);
